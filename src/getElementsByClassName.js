95081d04cbdf6c14002db6ee2863b1874145636b
Hack Reactor Student <student@hackreactor.com>
2017-06-26 12:32:58 -0700

    Working version of getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var returnArr = [];
+  var docBod = document.body;
+  var getElements = function(element, className) {
+    var childClasses = element.classList;
+    for (var key in childClasses) {
+      if (childClasses[key] === className) {
+        returnArr.push(element);
+      }
+    }
+    if (element.hasChildNodes()) {
+      var children = element.childNodes;
+      children.forEach(function(elem) {
+        getElements(elem, className);
+      });
+    }
+  };
+  getElements(docBod, className);
+  return returnArr;
 };
+
