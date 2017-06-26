d5664c29a95d403b4d6d163435a2d8575c811bf0
Hack Reactor Student <student@hackreactor.com>
2017-06-26 15:58:54 -0700

    "Started parseJSON, working on objects"

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,19 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  console.log(json)
+  if (json === '[]') {
+    return [];
+  }
+  if (json[0] === '{') {
+    var returnObj = {};
+    var key;
+    var value = false;
+    for (var i = 1; i < json.length; i++) {  
+      if (value === false && json[i] !== ':') {
+        key += json[i];
+      }
+      console.log(key)
+    }
+  }
 };
